3ea20be8411cacfacbd3806a71e169236bc15615
jjricks6 <jjricks1@gmail.com>
2021-12-10 12:15:31 -0700

    add hosted zone

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -192,6 +192,10 @@ resource "aws_api_gateway_stage" "magic_stage" {
   stage_name    = "prd"
 }
 
+resource "aws_route53_zone" "magic_zone" {
+  name = "8-ball.ml"
+}
+
 # resource "aws_api_gateway_domain_name" "magic_domain" {
 #   regional_certificate_arn = module.acs.certificate.arn
 #   domain_name              = "magic.byu-org-trn.amazon.byu.edu"
