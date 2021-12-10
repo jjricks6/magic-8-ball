70be0a25ea1a38fefb1236ea15de2b220d8ebd4b
jjricks6 <jjricks1@gmail.com>
2021-12-10 15:02:31 -0700

    fmt

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -241,12 +241,12 @@ resource "aws_s3_bucket" "magic_bucket" {
     }
   EOF
   cors_rule {
-        allowed_headers = ["*"]
-        allowed_methods = ["GET","POST"]
-        allowed_origins = ["*"]
-        expose_headers = ["ETag"]
-        max_age_seconds = 3000
-    }
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
 
   website {
     index_document = "index.html"
