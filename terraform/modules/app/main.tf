2bbfe7e7b5aea73e523429db8bb51f552b5818dd
jjricks6 <jjricks1@gmail.com>
2021-12-10 15:01:13 -0700

    add cors policy to bucket

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -240,6 +240,13 @@ resource "aws_s3_bucket" "magic_bucket" {
         ]
     }
   EOF
+  cors_rule {
+        allowed_headers = ["*"]
+        allowed_methods = ["GET","POST"]
+        allowed_origins = ["*"]
+        expose_headers = ["ETag"]
+        max_age_seconds = 3000
+    }
 
   website {
     index_document = "index.html"
